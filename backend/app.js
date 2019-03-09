2475924f750f70db9e92215e9902b6c16cac2972
dizotoff <izotov.dima@gmail.com>
2019-03-09 16:54:30 +0200

    boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const app = express()
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('', (req, res) => {
     res.send('Hello express!')
